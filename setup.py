64f0b057be6ca447c1fe7d590f95a54d3d8649ea
sjarvis1 <s.jarvis@imperial.ac.uk>
2013-04-18 17:36:53 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+#import os, sys
+from subprocess import call
+"""
+
+Sets up directory structures
+Runs nrnivmodl to generate objects from mod files
+"""
+
+# create folders
+folders = ['experiments/']
+for f in folders:
+    try:
+        call(['mkdir',f])
+        print 'Created directory', f
+    except:
+        pass
+    
+    
+# run nrnivmodl
+call(['nrnivmodl'])
