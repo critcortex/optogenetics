f5dd94dc9c382d0d62a322beded616e6aaca93cb
sjarvis1 <s.jarvis@imperial.ac.uk>
2015-03-16 15:49:24 +0000

    Added 99% changes required to do graded illumination. Have to alter opsin mod files to ensure that is fully working

--- src/opsin.py
+++ src/opsin.py
@@ -12,7 +12,12 @@ class Opsin:
             print 'Expressing opsin %s --------------------'%opsin
             for subdomain in params['opsindict'][opsin].keys():
                 print '- expressing %s in %s'%(opsin,subdomain)
-                total = 0
+                
+                # get the idx offset. This will only be used if setting unequal illumination
+                idx_offset = cell.get_idx(subdomain)[0]
+                print '_idx offset = ',idx_offset
+                
+                total = 0  # total sections expressed so far
                 #get corresponding section list
                 sectionlist = cell.domainlists[subdomain]
                 slcount= sectionlist.count()
@@ -21,7 +26,7 @@ class Opsin:
                     sec = sectionlist.o(i)
                     for (j,seg) in enumerate(sec):
                         #print type(seg), type(sec)
-                        op = self._express_opsin_section(seg,sec,opsin,params['opsindict'][opsin][subdomain])
+                        op = self._express_opsin_section(seg,sec,opsin,params['opsindict'][opsin][subdomain],idx_offset+total,cell)
                         oplist.append(op)
                         total +=1
                 print 'Expressed opsin %s in %g segments in subdomain %s'%(opsin,total,subdomain)
@@ -30,17 +35,45 @@ class Opsin:
         print '------ Finished expressing opsins ------'
         return expressed_op
     
+        
+    
     
 
-    def _express_opsin_section(self,segment,section,opsintype,opsindict):
+    def _express_opsin_section(self,segment,section,opsintype,opsindict,idx=None,cell=None,):
         """ Expresses opsin in a specific segment """
-
+            
         opsin = getattr(h,opsintype)()
-        opsin.loc(segment.x,sec=section)
+        opsin.loc(segment.x,sec=section) 
         opsin.gbar = opsindict['exp']*h.area(segment.x)
-                
+        
+        try: # see if we need to effective_illumination at each depth
+            projection = opsindict['projection']
+            if projection is not None and cell is not None and idx is not None:
+                if projection is 'z':
+                    mid_depth = cell.zmid[idx] 
+                    top_depth = cell.zmid.max()
+                elif projection is 'y':
+                    mid_depth = cell.ymid[idx] 
+                    top_depth = cell.ymid.max()
+                elif projection is 'x':
+                    mid_depth = cell.xmid[idx]
+                    top_depth = cell.xmid.max()
+            
+            effective_illumination = opsindict['irr_surface'] - opsindict['irr_gradient']*(top_depth-mid_depth)
+            if effective_illumination <0:
+                effective_illumination = 0
+            print mid_depth, (top_depth-mid_depth), effective_illumination
+            #opsin.irr_factor = effective_illumination
+            
+        except:
+            #print('!! Required information missing for calculating effective_illumination')
+            #print("!! Required fields for opdict = projection, irr_gradient, irr_surface")
+            pass
+            
+        
         for (k,v) in opsindict.iteritems():
-            if k=='exp':
+            # Note that we have a couple of special cases we have to ignore (and cannot delete from the dictionary)
+            if k in ['exp','irr_surface','irr_gradient','projection']:
                 continue
             try:
                 setattr(opsin,k,v)
